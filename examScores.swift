03d98c9e923f31f7a054116c4191e20da6b9678e
Medi W. Assumani <mediassumani49@gmail.com>
2018-01-12 17:46:53 -0500

    Update examScores.swift

--- examScores.swift
+++ examScores.swift
@@ -1,1 +1,13 @@
 
+
+/*
+ 
+                EXERCISE #3
+ 
+ Description : Given the exam grades of two classes : Find the the class that has the highest
+ 
+               Average and the common scores between the two classes
+ 
+ 
+ 
+ */
