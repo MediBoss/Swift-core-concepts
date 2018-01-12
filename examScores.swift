023434311f6d0f753c0c2f8ad5f09e1127589df0
Medi W. Assumani <mediassumani49@gmail.com>
2018-01-12 17:53:24 -0500

    Update examScores.swift

--- examScores.swift
+++ examScores.swift
@@ -1,5 +1,4 @@
 
-
 /*
  
                 EXERCISE #3
@@ -15,16 +14,28 @@
 var classOneGrades : [Double] = [89.8,77.4,56.9,90.8,97.7,67.2,64.0,70.7] // the test scores of the first class
 var classTwoGrades : [Double] = [78.3,98.5,86.2,79.0,100.0,78.5,56.0,63.9] // the test scores of the second class
 
+
             //Function to get Highest average score between the two classes to see wich one did better
-func getHighestAverage(classOne : [Double], clasTwo: [Double]) -> Double{
+func getHighestAverage(for classOne : [Double],for  clasTwo: [Double]) -> Double{
     
     
+    if getAverage(for: classOne) > getAverage(for: clasTwo){
+        
+        return getAverage(for: classOne)
+        
+    }else if getAverage(for: clasTwo) > getAverage(for: classOne){
+        
+        return getAverage(for: clasTwo)
+    }else{
+        
+        return 0.0
+    }
     
-    return 0
 }
 
+
         //Funtion to get the average score of an exam (in array)
-func getAverage(anyClass: [Double]) -> Double{
+func getAverage(for anyClass: [Double]) -> Double{
     
         var sum = 0.0
     
@@ -39,7 +50,7 @@ func getAverage(anyClass: [Double]) -> Double{
 
         //Function to get the coomon grades between two classes'cexam scores
 
-func getCommonScores(classOne: [Double], classTwo: [Double]) ->[Double]{
+func getCommonScores(for classOne: [Double], for classTwo: [Double]) ->[Double]{
     
    var commonGrades : [Double] = []
     
@@ -55,4 +66,3 @@ func getCommonScores(classOne: [Double], classTwo: [Double]) ->[Double]{
     
     return commonGrades
 }
-
