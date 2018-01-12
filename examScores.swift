5e94f1bc7ead51898f1fd863616443be6a063881
Medi W. Assumani <mediassumani49@gmail.com>
2018-01-12 17:47:32 -0500

    Update examScores.swift

--- examScores.swift
+++ examScores.swift
@@ -11,3 +11,6 @@
  
  
  */
+
+var classOneGrades : [Double] = [89.8,77.4,56.9,90.8,97.7,67.2,64.0,70.7] // the test scores of the first class
+var classTwoGrades : [Double] = [78.3,98.5,86.2,79.0,100.0,78.5,56.0,63.9] // the test scores of the second class
