63c03796a1b01e8282a9042a8c71cb8313af6ce4
Medi W. Assumani <mediassumani49@gmail.com>
2018-01-13 21:56:59 -0500

    Update classRosterChecking.swift

--- classRosterChecking.swift
+++ classRosterChecking.swift
@@ -1,1 +1,17 @@
 func checkRoster(for classDict : Dictionary<Int,String>,for studentId : Int) -> Bool{
+      //check if the id is valid
+    for id in classDict.keys{
+        
+        if id == studentId{
+            
+            return true //if it is valid, return true
+        }else{
+            
+            continue // we iterate until we find if studentId is in the dictionary's key
+        }
+        
+    }
+    
+    //if it's not valid, return false
+    return false
+}
