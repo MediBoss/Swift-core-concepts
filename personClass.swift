41bf1cadc1a82e7fd8abd506da4f9180899cf1fe
Medi W. Assumani <mediassumani49@gmail.com>
2018-01-17 20:10:34 -0500

    Update personClass.swift

--- personClass.swift
+++ personClass.swift
@@ -6,4 +6,25 @@ enum Gender{
     case Female
 }
 
+class Person{
+    
+    var fullName : String
+    var age : Int
+    var isMarried : Bool
+    var gender : Gender
+    
+    init(fullName: String, age : Int, isMarried : Bool, gender : Gender){
+        
+        self.fullName = fullName
+        self.age = age
+        self.isMarried = isMarried
+        self.gender = gender
+        
+        
+        
+    }
+    
+}
+
+
 
