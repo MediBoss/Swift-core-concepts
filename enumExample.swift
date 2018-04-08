ec9608eb0032d166e06695dab3d7f5b27a174238
Medi W. Assumani <mediassumani49@gmail.com>
2018-04-08 16:29:12 -0400

    Update enumExample.swift

--- enumExample.swift
+++ enumExample.swift
@@ -1,36 +1,27 @@
 
 import Foundation
 
-
-
-func canConnected(for connectivity : ConnectionState) -> Bool{
-    
-    var result = false
-    
-    switch connectivity{
-        
-    case .Connected(let port):
-        
-        if port == 3000{
-            
-            result = true
-        }
-        
-    default :
-        
-        result = false
-    }
-    
-    return reseult
-}
-
-
-
-enum ConnectionState : Int{
-    
+enum ConnectionState : Int{  
     case Connected = 2
     case Disconnected = 0
     case Connecting = 1
     case Unknown = -1
 }
+
+func canConnected(for connectivity : ConnectionState) -> Bool{
+    
+    var result = false
+
+    switch connectivity{   
+    case .Connected(let port):        
+        if port == 3000{
+            
+            result = true
+        }    
+    default :   
+        result = false
+    }
+    return reseult
+}
+
 
