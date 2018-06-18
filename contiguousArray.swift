567763c0f95855f79261a8a5be68cf1fb1eb3993
Medi Assumani <mediassumani49@gmail.com>
2018-06-17 20:40:57 -0400

    Update contiguousArray.swift

--- contiguousArray.swift
+++ contiguousArray.swift
@@ -1,3 +1,8 @@
+// Should be used when array's element are of a class type (e.g : not array of int, or string)
+// if element of array is of type struct or enum, same perfomance with Array
+// it is more efficinat when scalability and performance is the most important
+
+
 let list = ContiguousArray<Int>(1...100)
 
 for i in list{
