b794832760f4ac62f39ac59d709361347ace9d76
Medi Assumani <mediassumani49@gmail.com>
2018-06-30 20:47:49 -0400

    cleaning up

--- examScores.swift
+++ examScores.swift
@@ -1,6 +1,5 @@
 
 /*
- 
                 EXERCISE #3
  
  Description : Given the exam grades of two classes : Find the the class that has the highest
@@ -10,7 +9,6 @@
 
 var classOneGrades : [Double] = [89.8,77.4,56.9,90.8,97.7,67.2,64.0,70.7] // the test scores of the first class
 var classTwoGrades : [Double] = [78.3,98.5,86.2,79.0,100.0,78.5,56.0,63.9] // the test scores of the second class
-
 
             //Function to get Highest average score between the two classes to see wich one did better
 func getHighestAverage(for classOne : [Double],for  clasTwo: [Double]) -> Double{
@@ -27,7 +25,6 @@ func getHighestAverage(for classOne : [Double],for  clasTwo: [Double]) -> Double
         return 0.0
     }  
 }
-
         //Funtion to get the average score of an exam (in array)
 func getAverage(for anyClass: [Double]) -> Double{
     
@@ -37,22 +34,17 @@ func getAverage(for anyClass: [Double]) -> Double{
     }  
     return sum/Double(anyClass.count)    
 }
-
         //Function to get the coomon grades between two classes'cexam scores
 
 func getCommonScores(for classOne: [Double], for classTwo: [Double]) ->[Double]{
     
-   var commonGrades : [Double] = []
-    
-    for i in 0..<classOne.count{
-        
-        if classOne[i] == classTwo[i]{
-            
+   var commonGrades : [Double] = [] 
+    for i in 0..<classOne.count{   
+        if classOne[i] == classTwo[i]{        
             commonGrades[i] = classOne[i]
         }else{
             continue
         }
-    }
-    
+    } 
     return commonGrades
 }
