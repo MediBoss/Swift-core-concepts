3996ed9fa0a43b75058d4c101b800eef262c35a1
Medi Assumani <mediassumani49@gmail.com>
2018-09-04 13:47:46 -0700

    Update array.swift

--- array.swift
+++ array.swift
@@ -1,1 +1,10 @@
 
+var movies = [String]() // empty array
+var movies: [String] = ["Jurastic parl", "Thor Ragnarock"]// array litteral
+
+movie.count // counts the elements
+movie.append("value") // adds value
+movie.insertAtIndex("", at : 0) // add at specific index
+movie.removeAt
+
+movie[0] = "movie"
