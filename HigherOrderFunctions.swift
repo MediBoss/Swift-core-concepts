e5c416a2914f39c2ef01ada2717131e407c04d6a
Medi Assumani <mediassumani49@gmail.com>
2019-08-28 11:39:59 -0700

    Update HigherOrderFunctions.swift

--- HigherOrderFunctions.swift
+++ HigherOrderFunctions.swift
@@ -2,12 +2,17 @@
 //: Playground - noun: a place where people can play
 
 /*
-        THREE MAIN HIGHER ORDER FUNTIONS
-1. Map : using the Map function to perform the same operation to all elements\
-2. Filter : Used to filter out the collection based on some requirements
-3. FlatMap : Flatens the collection, and gets rid of nil values
+        THREE MAIN HIGHER ORDER FUNCTIONS
+1. Map: using the Map function to perform the same operation to all elements\
+2. Filter: Used to filter out the collection based on some requirements
+3. FlatMap: Flatens the collection, and gets rid of nil values
+4. Combine
+5. DistinctUntilChanged
+6. Buffer
+7. Scan
+8. Zip
 
-NOTE : Before you can use them , make sure you can at least perform those operations logically(wthe regular way)
+NOTE: Before you can use them , make sure you can at least perform those operations logically(wthe regular way)
 **/
 
 import Foundation
@@ -16,7 +21,8 @@ let testScores: [Int] = [90,78,76,83, 84, 87,10,40,34,94,65,90,55,77,73,98]
 print(testScores.map {$0 + 2 })
 print(testScores.map {$0 * 2})
 
-        // Maping a Dictionary
+        // Map
+
 let topSchools: Dictionary<String,Int> = ["NJIT" : 23000, "Columbia" : 45000, "Product College": 300, "University of Minnessota" : 13000]
 
 // Increment each University's student body by 10
@@ -28,7 +34,8 @@ print(testScores.filter { $0 > 80 })
 print(topSchools.filter { $1 < 500}) 
 print(testScores.flatMap { $0 })
 
-                        // Using reduce
+                        // Reduce
+
 //The reduce function allows you to combine all the elements in an array and return an object of any type (generics!!!!)
 var strings: [String] = ["Another", "Brick", "In", "The", "Wall"]
 
